6bbc5476fe04559cc1c72051ef0795d1de11147e
Juan Rojas <juanrojas1912@gmail.com>
2022-09-21 18:32:32 -0400

    todo list project init

--- 05_dom-manipulation-form/app.js
+++ 05_dom-manipulation-form/app.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log('Connected ... ')
+console.log('Connected ... ');
+
+const form = document.querySelector('#logoForm');
+const brandInput = form.querySelector('#brand-name');
+const brandColor = form.querySelector('#brand-color');
+const fontSize = form.querySelector('#font-size');
+
+console.log(brandInput);
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  console.log(brandColor.value);
+})
