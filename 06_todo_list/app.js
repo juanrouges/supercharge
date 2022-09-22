83d3d24a170eafda0e9905ba284522fda45a2c85
Juan Rojas <juanrojas1912@gmail.com>
2022-09-21 22:51:02 -0400

    todo list

--- 06_todo_list/app.js
+++ 06_todo_list/app.js
@@ -2,3 +2,37 @@
 
 console.log('Connected ...');
 
+// Elements
+const addItemForm = document.querySelector('#addItemForm');
+const todoList = document.querySelector('#todoList');
+
+// Form Submit Add New 
+addItemForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  let target = event.currentTarget;
+  let todoItem = document.createElement('li');
+  todoItem.classList.add('todo_item');
+  let todoItemText = document.createElement('span');
+  todoItemText.innerText = target.querySelector('input').value;
+  todoItem.append(todoItemText);
+  let deleteButtonItem = document.createElement('button');
+  deleteButtonItem.innerHTML = '&#10005;';
+  todoItem.append(deleteButtonItem);
+  let checkboxItem = document.createElement('input');
+  checkboxItem.setAttribute('type', 'checkbox');
+  todoItem.prepend(checkboxItem);
+  todoList.append(todoItem);
+  target.querySelector('input').value = '';
+});
+
+// List event listeners (done and remove)
+todoList.addEventListener('click', function(event) {
+  console.log(event.target.parentElement);
+  if (event.target.tagName === "INPUT") {
+    event.target.parentElement.classList.toggle('crossout');
+  } 
+  else if (event.target.tagName === "BUTTON") {
+    event.target.parentElement.remove();
+  }
+});
+
