1566a54645fb232aa23f1e73935a7bcee6470bc0
Juan Rojas <juanrojas1912@gmail.com>
2022-09-23 17:33:40 -0400

    todo save in localStorage

--- 06_todo_list/app.js
+++ 06_todo_list/app.js
@@ -6,23 +6,51 @@ console.log('Connected ...');
 const addItemForm = document.querySelector('#addItemForm');
 const todoList = document.querySelector('#todoList');
 
-let array = [
-  {
-    completed: false,
-    task: 'Lorem ipsum dirono'
-  },
-  {
-    completed: true,
-    task: 'Romulo de la casarena ilinois'
+// let array = [
+//   {
+//     completed: false,
+//     task: 'Lorem ipsum dirono'
+//   },
+//   {
+//     completed: true,
+//     task: 'Romulo de la casarena ilinois'
+//   }
+// ];
+
+// localStorage.setItem('todo', JSON.stringify(array));
+
+let todo = JSON.parse(localStorage.getItem('todo')) || [];
+console.log(todo.length);
+
+function uid() {
+  let a = new Uint32Array(3);
+  window.crypto.getRandomValues(a);
+  return (performance.now().toString(36)+Array.from(a).map(A => A.toString(36)).join("")).replace(/\./g,"");
+};
+
+// let test 
+
+console.log(uid());
+
+if (todo.length > 0) {
+  console.log(todo);
+
+  for (const index of todo) {
+    console.log(index.task);
+
+    let todoItem = document.createElement('li');
+    todoItem.classList.add('todo_item');
+    let todoItemText = document.createElement('span');
+    todoItemText.innerText = index.task;
+    todoItem.append(todoItemText);
+    let deleteButtonItem = document.createElement('button');
+    deleteButtonItem.innerHTML = '&#10005;';
+    todoItem.append(deleteButtonItem);
+    let checkboxItem = document.createElement('input');
+    checkboxItem.setAttribute('type', 'checkbox');
+    todoItem.prepend(checkboxItem);
+    todoList.append(todoItem);
   }
-];
-
-localStorage.setItem('todo', JSON.stringify(array));
-
-let todo = JSON.parse(localStorage.getItem('todo'));
-
-for (const index of todo) {
-  console.log(index.task);
 }
 
 // Form Submit Add New 
@@ -43,9 +71,11 @@ addItemForm.addEventListener('submit', function(event) {
   todoList.append(todoItem);
   target.querySelector('input').value = '';
 
-  let omega = {completed: false, task: 'My latino ass'};
+  let omega = {completed: false, task: todoItemText.innerText};
+
+  todo.push(omega);
   
-  localStorage.setItem('todo', JSON.stringify(omega));
+  localStorage.setItem('todo', JSON.stringify(todo));
 });
 
 // List event listeners (done and remove)
